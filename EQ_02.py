1b2d8b53c34a356087f0c4f8b6b0c19e16daa656
pranav0904 <pranavkhiste1@gmail.com>
2020-11-22 23:29:37 -0500

    Create EQ_02.py

---
+++ EQ_02.py
@@ -0,0 +1,6 @@
+from EQ_01 import portfolio_return
+
+print("EQ_02")
+Stock_symbols = ['JILL','ELTK','ONVO','UAVS', 'AEY','OBLN']
+
+print(portfolio_return(Stock_symbols, '2019-06-28', '2019-08-31'))
